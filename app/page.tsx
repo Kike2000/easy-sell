43ff0fea0aa57adc115978cc8acc06bd8a1e0b4a
Kike2000 <72471132+Kike2000@users.noreply.github.com>
2024-11-12 21:28:36 -0600

    feat: Making a test

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     {
       id: 0,
       name: "Mushroom Orange Lamp",
-      description: "Test",
+      description: "Test2",
       price: 40,
       imageUrl:
         "https://res.cloudinary.com/mullanlighting/c_limit,dpr_2.0,f_auto,h_567,q_80,w_567/media/catalog/product/m/l/mlbwl141antbrs_3_1.jpg",
